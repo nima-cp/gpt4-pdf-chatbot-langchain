4014fb222890b0e16552f8696cbea0e9993324ef
nima-cp <nima.k@centropaghe.it>
2023-07-31 17:09:07 +0200

    feat: responds in any language

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,27 +2,27 @@ import { OpenAI } from 'langchain/llms/openai';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 import { Chroma } from 'langchain/vectorstores/chroma';
 
-const CONDENSE_PROMPT = `Data la seguente conversazione e una domanda di follow-up, riformula la domanda di follow-up in modo che sia una domanda a sé stante.
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
-Cronologia chat:
+Chat History:
 {chat_history}
-Input di follow-up: {question}
-Domanda autonoma:`;
+Follow Up Input: {question}
+Standalone question:`;
 
-const QA_PROMPT = `Sei un utile assistente AI. Usa i seguenti elementi di contesto per rispondere alla domanda alla fine.
-Se non conosci la risposta, dì semplicemente che non lo sai. NON cercare di inventare una risposta.
-Se la domanda non è correlata al contesto, rispondi educatamente che sei sintonizzato per rispondere solo a domande correlate al contesto.
+const QA_PROMPT = `You are a helpful multilingual AI assistant. Use the following pieces of context to answer in the same language as the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
 
-Domanda: {question}
-Risposta utile in markdown:`;
+Question: {question}
+Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: Chroma) => {
   const model = new OpenAI({
     temperature: 0, // increase temperature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
-    // modelName: 'gpt-3.5-turbo-0613',
+    // modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    modelName: 'gpt-3.5-turbo-0613',
     //modelName: 'gpt-4',
   });
 
