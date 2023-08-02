aa5056919ada1ac9fd7b306b9f4e569200694948
nima-cp <nima.k@centropaghe.it>
2023-08-02 10:24:33 +0200

    solo with JS

--- scripts/faiss-load.ts
+++ scripts/faiss-load.ts
@@ -1,15 +1,25 @@
 import { FaissStore } from 'langchain/vectorstores/faiss';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
-// The directory of data saved from Python
-const directory = '/db/vectordb/faiss_index';
+const filePath = 'docs';
+const directoryLoader = new DirectoryLoader(filePath, {
+  '.pdf': (path) => new PDFLoader(path),
+});
+// Create docs with a loader
+const rawDocs = await directoryLoader.load();
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
+const docs = await textSplitter.splitDocuments(rawDocs);
 
-// // Load the vector store from the directory
-const loadedVectorStore = await FaissStore.loadFromPython(
-  directory,
-  new OpenAIEmbeddings(),
-);
+// Load the docs into the vector store
+const embedder = new OpenAIEmbeddings();
+const vectorStore = await FaissStore.fromDocuments(docs, embedder);
 
 // Search for the most similar document
-const result = await loadedVectorStore.similaritySearch('test', 2);
-console.log('result', result);
+const resultOne = await vectorStore.similaritySearchWithScore('date', 3);
+console.log(resultOne);
