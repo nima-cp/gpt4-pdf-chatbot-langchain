82cadebc84bde652f276f71386d7ee63ce2847c9
nima-cp <nima.k@centropaghe.it>
2023-08-04 16:57:12 +0200

    feat: conversation with chat history

--- utils/chain.py
+++ utils/chain.py
@@ -13,9 +13,7 @@ from langchain.embeddings import SentenceTransformerEmbeddings
 # %%
 
 embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-
 persist_directory = "../db/vectorStore/chromadb"
-
 vectorStore = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 
 
@@ -43,29 +41,85 @@ vectorStoreRetriever = vectorStore.as_retriever(search_kwargs={"k": 3})
 # %%
 
 
-# Build prompt
-from langchain.prompts import PromptTemplate
+# # Build prompt
+# from langchain.prompts import PromptTemplate
 
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible.
-{context}
-Question: {question}
-Helpful Answer:"""
-QA_CHAIN_PROMPT = PromptTemplate(
-    input_variables=["context", "question"],
-    template=template,
-)
-# %%
-# Run chain
-# question = "what is random forest"
+# template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible.
+# {context}
+# Question: {question}
+# Helpful Answer:"""
+# QA_CHAIN_PROMPT = PromptTemplate(
+#     input_variables=["context", "question"],
+#     template=template,
+# )
+# # %%
+# # Run chain
+# # question = "what is random forest"
 
-qa_chain = RetrievalQA.from_chain_type(
-    llm,
+# qa_chain = RetrievalQA.from_chain_type(
+#     llm,
+#     retriever=vectorStoreRetriever,
+#     chain_type="stuff",
+#     return_source_documents=True,
+#     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+# )
+
+# result = qa_chain({"query": question})
+# print(result)
+# print(result["result"])
+
+
+# %% conversational Retrieval Chain
+from langchain.memory import ConversationBufferMemory
+
+# memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+conversational_chain = ConversationalRetrievalChain.from_llm(
+    llm=llm,
     retriever=vectorStoreRetriever,
+    # memory=memory,
     chain_type="stuff",
     return_source_documents=True,
-    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+    return_generated_question=True,
 )
 
-result = qa_chain({"query": question})
+chat_history = []
+question = "what is the iban of WORLD FUEL SERVICE SINGAPORE5"
+result = conversational_chain({"question": question, "chat_history": chat_history})
+# result = conversational_chain({"question": question})
 print(result)
-print(result["result"])
+print(result["answer"])
+
+# %%
+chat_history.append((question, result["answer"]))
+question = "can you explain more about it?"
+
+result = conversational_chain({"question": question, "chat_history": chat_history})
+print(result)
+print(result["answer"])
+
+
+# %%
+## Cite sources
+def process_llm_response(llm_response):
+    print(llm_response["answer"])
+    print("\n\nSources:")
+    for source in llm_response["source_documents"]:
+        print(source.metadata["source"])
+
+
+# %%
+# example
+question = "what is the iban of WORLD FUEL SERVICE SINGAPORE5"
+llm_response = conversational_chain(
+    {"question": question, "chat_history": chat_history}
+)
+process_llm_response(llm_response)
+
+# %%
+# break it down
+question = "hi?"
+llm_response = conversational_chain(
+    {"question": question, "chat_history": chat_history}
+)
+process_llm_response(llm_response)
